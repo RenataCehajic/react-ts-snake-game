d789236c8bfddefb3d4141ba7c4d66e3392fe373
RenataCehajic <renata.cehajic@gmail.com>
2022-02-16 16:54:24 +0100

    Making snake to move

--- src/App.js
+++ src/App.js
@@ -14,19 +14,37 @@ const App = () => {
   const [snake, setSnake] = useState(SNAKE_START);
   const [apple, setApple] = useState(APPLE_START);
   const [dir, setDir] = useState([0, -1]);
-  const [speed, setSpeed] = useState(1000);
+  const [speed, setSpeed] = useState(null);
   const [gameOver, setGameover] = useState(false);
 
-  const startGame = () => {};
+  const startGame = () => {
+    setSnake(SNAKE_START);
+    setApple(APPLE_START);
+    setDir([0, -1]);
+    setSpeed(SPEED);
+    setGameover(false);
+  };
 
-  const endGame = () => {};
+  const endGame = () => {
+    setSpeed(null);
+    setGameover(true);
+  };
 
   const moveSnake = ({ keyCode }) =>
     keyCode >= 37 && keyCode <= 40 && setDir(DIRECTIONS[keyCode]);
 
   const createApple = () => {};
 
-  const checkCollision = () => {};
+  const checkCollision = (piece, snk = snake) => {
+    if (
+      piece[0] * SCALE >= CANVAS_SIZE[0] ||
+      piece[0] < 0 ||
+      piece[1] * SCALE >= CANVAS_SIZE[1] ||
+      piece[1] < 0
+    )
+      return true;
+    return false;
+  };
 
   const checkAppleCollision = () => {};
 
@@ -34,6 +52,7 @@ const App = () => {
     const snakeCopy = JSON.parse(JSON.stringify(snake));
     const newSnakeHead = [snakeCopy[0][0] + dir[0], snakeCopy[0][1] + dir[1]];
     snakeCopy.unshift(newSnakeHead);
+    if (checkCollision(newSnakeHead)) endGame();
     snakeCopy.pop();
     setSnake(snakeCopy);
   };
