c88c075ab7e15436a0e00b3cb0c4a1043a675520
RenataCehajic <renata.cehajic@gmail.com>
2022-02-16 16:30:22 +0100

    Add useInterval function

--- src/App.js
+++ src/App.js
@@ -14,14 +14,15 @@ const App = () => {
   const [snake, setSnake] = useState(SNAKE_START);
   const [apple, setApple] = useState(APPLE_START);
   const [dir, setDir] = useState([0, -1]);
-  const [speed, setSpeed] = useState(null);
+  const [speed, setSpeed] = useState(1000);
   const [gameOver, setGameover] = useState(false);
 
   const startGame = () => {};
 
   const endGame = () => {};
 
-  const moveSnake = () => {};
+  const moveSnake = ({ keyCode }) =>
+    keyCode >= 37 && keyCode <= 40 && setDir(DIRECTIONS[keyCode]);
 
   const createApple = () => {};
 
@@ -29,7 +30,13 @@ const App = () => {
 
   const checkAppleCollision = () => {};
 
-  const gameLoop = () => {};
+  const gameLoop = () => {
+    const snakeCopy = JSON.parse(JSON.stringify(snake));
+    const newSnakeHead = [snakeCopy[0][0] + dir[0], snakeCopy[0][1] + dir[1]];
+    snakeCopy.unshift(newSnakeHead);
+    snakeCopy.pop();
+    setSnake(snakeCopy);
+  };
 
   useEffect(() => {
     const context = canvasRef.current.getContext("2d");
@@ -37,7 +44,11 @@ const App = () => {
     context.clearRect(0, 0, CANVAS_SIZE[0], CANVAS_SIZE[1]);
     context.fillStyle = "pink";
     snake.forEach(([x, y]) => context.fillRect(x, y, 1, 1));
+    context.fillStyle = "lightblue";
+    context.fillRect(apple[0], apple[1], 1, 1);
   }, [snake, apple, gameOver]);
+
+  useInterval(() => gameLoop(), speed);
 
   return (
     <div role="button" tabIndex="0" onKeyDown={(e) => moveSnake(e)}>
